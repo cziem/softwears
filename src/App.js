d968649b912019baedd6e6b39f1571bdd0739056
Favour George C <phavorsparks@gmail.com>
2019-09-17 01:33:50 +0100

    update with homepage and sass styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homepage'
+
 function App() {
   return (
     <div>
-      Hello world
+      <HomePage />
     </div>
   );
 }
