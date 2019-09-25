9cf61ddbf5f1624e2d83c351e82b61cee525be18
Favour George C <phavorsparks@gmail.com>
2019-09-25 23:09:03 +0100

    add carticon to the header component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 
 import { auth } from '../../firebase/firebase.utils'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
+import CartIcon from '../cart-icon/CartIcon'
 
 import './header.scss'
 
@@ -26,6 +27,7 @@ const Header = ({ currentUser }) => (
             <Link className="option" to="/signin">SIGN IN</Link>
           )
       }
+      <CartIcon />
     </div>
   </div>
 )
