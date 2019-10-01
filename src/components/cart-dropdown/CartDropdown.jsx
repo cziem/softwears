d284265dae685f693e6827633f31ecea1ee4e322
Favour George C <phavorsparks@gmail.com>
2019-10-01 09:27:44 +0100

    update

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -24,7 +24,7 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
     </div>
     <CustomButton onClick={() => {
       history.push('/checkout')
-      dispatch(toggleCartHidden())
+      dispatch(toggleCartHidden)
     }}>GO TO CHECKOUT</CustomButton>
   </div>
 )
